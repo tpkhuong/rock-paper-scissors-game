52ef73cd340397742c47ff8df3bde79bf6765d39
Toan <tpkhuong@gmail.com>
2023-01-25 12:46:43 -0500

    changed filename for output and add production code in dist folder

--- webpack.config.common.js
+++ webpack.config.common.js
@@ -16,7 +16,7 @@ module.exports = {
   },
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "[name].bundle.js",
+    filename: "[name]-bundle.[contenthash].js",
   },
   plugins: [
     new HtmlWebpackPlugin({ template: "./src/index.html" }),
